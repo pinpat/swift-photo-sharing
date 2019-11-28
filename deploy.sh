e97433c2e148987c88303c4db068316d8865b200
vlad1830 <mpspider104@gmail.com>
2019-11-28 10:56:19 +0700

    add deploy

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+cd ./server && npm run build && cd ..
+scp -i ./private.pem -r ./server/dist ec2-user@3.212.245.106:/var/www/yaad/
+scp -i ./private.pem ./server/Dockerfile ec2-user@3.212.245.106:/var/www/yaad/
+scp -i ./private.pem ./server/docker-compose.yml ec2-user@3.212.245.106:/var/www/yaad/
+scp -i ./private.pem ./server/package.json ec2-user@3.212.245.106:/var/www/yaad/
+scp -i ./private.pem ./server/tsconfig.json ec2-user@3.212.245.106:/var/www/yaad/
+scp -i ./private.pem ./server/ormconfig.js ec2-user@3.212.245.106:/var/www/yaad/
